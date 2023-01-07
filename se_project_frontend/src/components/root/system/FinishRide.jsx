5cb42e44cd0287bb4b93c9a93d2cb38351e68a21
radu_vele <99049451+Radu-Vele@users.noreply.github.com>
2023-01-07 10:36:50 +0200

    added control over end ride page

--- se_project_frontend/src/components/root/system/FinishRide.jsx
+++ se_project_frontend/src/components/root/system/FinishRide.jsx
@@ -2,16 +2,32 @@ import React from "react";
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import { Typography } from '@mui/material';
-import { useState } from 'react';
+import { useState, useLayoutEffect } from 'react';
 import { useEffect } from 'react';
 import FinishRideService from "../../../api/system/FinishRideService";
 import AuthenticationService from "../../../api/authentication/AuthenticationService";
+import UserDetailsService from "../../../api/users/UserDetailsService";
 
 const FinishRide = () => {
     const [date, setDate] = useState(new Date());
-    const [error, setError] = useState({});
+    const [error, setError] = useState(false);
     const [success, setSuccess] = useState(false);
-    
+    const [user, setUser] = useState();
+    const tempHasActiveRide = true;
+
+    useLayoutEffect(() => {
+        let unmounted = false;
+      
+        UserDetailsService().then((response) => {
+          if(!unmounted) {
+              setUser(response.data);
+          }
+        });
+        return () => {
+          unmounted = true;
+        };
+      }, [])
+
     const username = AuthenticationService.getLoggedInUser();
 
     useEffect( () => {
@@ -49,12 +65,13 @@ const FinishRide = () => {
         p={2}
         textAlign="center"
         sx={{ width: 500 }}
-
         >
+        {tempHasActiveRide && (
+        <div>      
             <Typography variant="h5">
                 Your Current Ride
-            </Typography>
-            
+            </Typography> 
+
             <br></br>
 
             <Typography >
@@ -65,11 +82,27 @@ const FinishRide = () => {
 
             <Button
                 variant="contained"
-                color="success"
                 onClick={handleFinishRide}
             >
                 End Ride
             </Button>
+
+            <Typography hidden={!error} color="error">
+                Failed request!
+            </Typography>
+
+            <Typography hidden={!success} color="success">
+                The ride was ended and saved successfully!
+            </Typography>
+        </div>
+        )}
+        {!tempHasActiveRide && (
+        <div>
+            <Typography color="error" variant="h5">
+                You have no active ride!
+            </Typography>
+        </div>
+        )}
     </Box>
     );
 }
