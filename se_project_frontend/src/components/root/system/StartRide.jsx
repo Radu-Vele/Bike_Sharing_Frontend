864bdd6df901e449047545c22297b3e1a824a938
andreea păun minunata <andreeapaun321@gmail.com>
2023-01-08 20:19:22 +0200

    fixed form

--- se_project_frontend/src/components/root/system/StartRide.jsx
+++ se_project_frontend/src/components/root/system/StartRide.jsx
@@ -16,12 +16,16 @@ const StartRide = () => {
 
     // const [loadingStartStations, setLoadingStartStations] = useState(true);
     // const [loadingUsableBikes, setLoadingUsableBikes] = useState(true);
-    
-    const [chosenStation, setChosenStation] = useState("");
+
+    const [chosenStation, setChosenStation] = useState({});
     const [chosenBike, setChosenBike] = useState("");
     const [chosenEnd, setChosenEnd] = useState("");
 
+    var chosenStationId;
+    var chosenEndId;
+
     const [stationData, setStationData] = useState([]);
+    const [endStationData, setEndStationData] = useState([]);
 
     const [bikeData, setBikeData] = useState([]);
     const username = AuthenticationService.getLoggedInUser();
@@ -29,8 +33,8 @@ const StartRide = () => {
     const [info, setInfo] = useState({
         username: username,
         startStationId: "",
-        bikeId: "",
-        endStationId: ""
+        endStationId: "",
+        bikeId: ""
     });
 
     const [success, setSuccess] = useState(false);
@@ -39,25 +43,25 @@ const StartRide = () => {
     const validate = () => {
         const errors = {};
 
-        if (!info.startStationId) {
-            errors.startStationId_error = true;
-            errors.startStationId = "Please choose a start station!";
-        }
+        // if (!info.startStationId) {
+        //     errors.startStationId_error = true;
+        //     errors.startStationId = "Please choose a start station!";
+        // }
 
         if (!info.bikeId) {
             errors.bikeId_error = true;
             errors.bikeId = "Please choose a bike from your start station!";
         }
 
-        if (!info.endStationId) {
-            errors.endStationId_error = true;
-            errors.endStationId = "Please choose an end station!";
-        }
+        // if (!info.endStationId) {
+        //     errors.endStationId_error = true;
+        //     errors.endStationId = "Please choose an end station!";
+        // }
 
-        return errors;            
+        return errors;
     };
 
-    const getStations = async() => {
+    const getStations = async () => {
         try {
             const response = await axios.get("/get-usable-stations");
             let arr = [];
@@ -69,7 +73,7 @@ const StartRide = () => {
                         id: iterator.id
                     };
 
-                    arr.push(temp);             
+                    arr.push(temp);
                 }
                 setStationData(arr);
                 console.log(stationData);
@@ -77,57 +81,95 @@ const StartRide = () => {
         }
         catch (err) {
             let error = "";
-            if(err.response) {
+            if (err.response) {
                 error += err.response;
             }
             return error;
         }
 
-        };
+    };
 
-        const getBikes = async() => {
-            try {
-                
-                console.log("got here")
-                const response = await axios.get("/get-usable-bikes", {params: { stationId: this.chosenStation } });
-                console.log("got here x2")
-                let arr = [];
-                if (response.data.length > 0) {
-                    let availableUsableBikes = response.data;
-                    for (const iterator of availableUsableBikes) {
-                        //let temp = iterator.id;
-                        arr.push(iterator.id);             
-                    }
-                    setBikeData(arr);
-                    console.log(bikeData);
+    const getBikes = async () => {
+        try {
+            for (const iterator of stationData) {
+                if (iterator.name === chosenStation) {
+                    chosenStationId = iterator.id;
+                    console.log(chosenStationId)
                 }
             }
-            catch (err) {
-                let error = "";
-                if(err.response) {
-                    error += err.response;
+            const response = await axios.get("/get-usable-bikes?stationId=" + chosenStationId);
+            console.log("got here x2")
+            let arr = [];
+            if (response.data.length > 0) {
+                let availableUsableBikes = response.data;
+                for (const iterator of availableUsableBikes) {
+                    //let temp = iterator.id;
+                    arr.push(iterator.id);
                 }
-                return error;
+                setBikeData(arr);
+                console.log(bikeData);
             }
-    
-            };
+        }
+        catch (err) {
+            let error = "";
+            if (err.response) {
+                error += err.response;
+            }
+            return error;
+        }
+
+    };
+
+    const getEndStations = async () => {
+        try {
+            const response = await axios.get("/get-free-stations");
+            let arr = [];
+            if (response.data.length > 0) {
+                let availableEndStations = response.data;
+                for (const iterator of availableEndStations) {
+                    let temp = {
+                        name: iterator.name,
+                        id: iterator.id
+                    };
+
+                    arr.push(temp);
+                }
+                setEndStationData(arr);
+                console.log(endStationData);
+            }
+        }
+        catch (err) {
+            let error = "";
+            if (err.response) {
+                error += err.response;
+            }
+            return error;
+        }
+
+    };
 
     const submitHandler = async (event) => {
+        info.startStationId = chosenStationId
+        for (const iterator of endStationData) {
+            if (iterator.name === chosenEnd) {
+                chosenEndId = iterator.id;
+                console.log(chosenEndId)
+            }
+        }
+        info.endStationId = chosenEndId
+
+        console.log("got here" + info.startStationId)
         event.preventDefault();
 
         let errors = validate(info);
         setErrors(errors);
 
-        if(Object.keys(errors).length === 0) {
-
-            setInfo.startStationId(chosenStation);
-            info.bikeId = chosenBike;
-            info.endStationId = chosenEnd;
+        if (Object.keys(errors).length === 0) {
             const response = await StartRideService(info);
             if (response.status === 201) {
             }
             else {
-              console.log(errors);
+                console.log(errors);
             }
 
         }
@@ -141,58 +183,83 @@ const StartRide = () => {
 
     const handleChange2 = (event) => {
         setChosenBike(event.target.value);
+        setInfo({ ...info, bikeId: event.target.value })
         console.log(event.target.value);
     }
 
+    const handleChange3 = (event) => {
+        setChosenEnd(event.target.value);
+        console.log(event.target.value);
+    }
+
+
     const menuItems = stationData.map(item => (
         <MenuItem key={item.id} value={item.name}>{item.name}</MenuItem>
-      ));
+    ));
 
-      const menuItemsBikes = bikeData.map(item => (
-        <MenuItem>{item.id}</MenuItem>
-      ));      
+    const menuItemsBikes = bikeData.map((item, i) => (
+        <MenuItem key={i} value={(i + 1) * 10}>{item}</MenuItem>
+    ));
+
+    const menuItemsEnd = endStationData.map(item => (
+        <MenuItem key={item.id} value={item.name}>{item.name}</MenuItem>
+    ));
 
     return (
-        <Container variant="main" maxWidth = "xl">
-        <FormControl sx={{ m: 1, minWidth: 300}}>
-            <InputLabel id="chosen-station-id">Start station</InputLabel>
-            <Select
-                labelId="chosen-station-id"
-                id="chosen-station"
-                value={chosenStation}
-                label="Choose station"
-                onOpen={getStations}
-                onChange={handleChange1}
-            >
-                {menuItems}
-            </Select>
-        </FormControl>
-        <br></br>
-        <FormControl sx={{ m: 1, minWidth: 300}}>
-            <InputLabel id="chosen-bike-id">Choose a bike</InputLabel>
-            <Select
-                labelId="chosen-bike-id"
-                id="chosen-bike"
-                value={chosenBike}
-                label="Choose bike"
-                onOpen={getBikes}
-                onChange={handleChange2}
-            >
-                {menuItemsBikes}
-            </Select>
-        </FormControl>
-        <br></br>
-        <FormControl sx={{ m: 1, minWidth: 300}}>
-            <Button
-                onClick={submitHandler}
-                variant="contained"
-                color="secondary"
-            >
-                Start Your Ride
-            </Button> 
-        </FormControl>
+        <Container variant="main" maxWidth="xl">
+            <FormControl sx={{ m: 1, minWidth: 300 }}>
+                <InputLabel id="chosen-station-id">Start station</InputLabel>
+                <Select
+                    labelId="chosen-station-id"
+                    id="chosen-station"
+                    value={chosenStation}
+                    label="Choose station"
+                    onOpen={getStations}
+                    onChange={handleChange1}
+                >
+                    {menuItems}
+                </Select>
+            </FormControl>
+            <br></br>
+            <FormControl sx={{ m: 1, minWidth: 300 }}>
+                <InputLabel id="chosen-bike-id">Choose bike</InputLabel>
+                <Select
+                    labelId="chosen-bike-id"
+                    id="chosen-bike"
+                    value={chosenBike}
+                    label="Choose bike"
+                    onOpen={getBikes}
+                    onChange={handleChange2}
+                >
+                    {menuItemsBikes}
+                </Select>
+            </FormControl>
+            <br></br>
+            <FormControl sx={{ m: 1, minWidth: 300 }}>
+                <InputLabel id="chosen-end-station-id">End station</InputLabel>
+                <Select
+                    labelId="chosen-end-station-id"
+                    id="chosen-end-station"
+                    value={chosenEnd}
+                    label="Choose station"
+                    onOpen={getEndStations}
+                    onChange={handleChange3}
+                >
+                    {menuItemsEnd}
+                </Select>
+            </FormControl>
+            <br></br>
+            <FormControl sx={{ m: 1, minWidth: 300 }}>
+                <Button
+                    onClick={submitHandler}
+                    variant="contained"
+                    color="secondary"
+                >
+                    Start Your Ride
+                </Button>
+            </FormControl>
         </Container>
-      );
+    );
 }
 
 export default StartRide;
